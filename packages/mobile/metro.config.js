f9436c697f5587f25f6590239a30fee222f50816
Bruno Lemos <brunohplemos@gmail.com>
2019-03-13 15:03:36 -0300

    Upgrade react-native to v0.59.0 stable
    The projectRoot step from the tutorial has changed: https://dev.to/brunolemos/tutorial-100-code-sharing-between-ios-android--web-using-react-native-web-andmonorepo-4pej

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -1,11 +1,7 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+const path = require('path')
 
 module.exports = {
+  projectRoot: path.resolve(__dirname, '../../'),
   transformer: {
     getTransformOptions: async () => ({
       transform: {
@@ -14,4 +10,4 @@ module.exports = {
       },
     }),
   },
-};
+}
