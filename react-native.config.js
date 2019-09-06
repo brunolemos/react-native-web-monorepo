6b5c0815abc0c366d88d3f2e2075c456fbebb2bd
Bruno Lemos <brunohplemos@gmail.com>
2019-09-06 19:58:50 -0300

    #

---
+++ react-native.config.js
@@ -0,0 +1,23 @@
+const ios = require('@react-native-community/cli-platform-ios');
+const android = require('@react-native-community/cli-platform-android');
+
+module.exports = {
+  platforms: {
+    ios: {
+      linkConfig: ios.linkConfig,
+      projectConfig: ios.projectConfig,
+      dependencyConfig: ios.dependencyConfig,
+    },
+    android: {
+      linkConfig: android.linkConfig,
+      projectConfig: android.projectConfig,
+      dependencyConfig: android.dependencyConfig,
+    },
+  },
+  project: {
+    android: {
+      sourceDir: './packages/mobile/android',
+    },
+    ios: {}
+  },
+};
