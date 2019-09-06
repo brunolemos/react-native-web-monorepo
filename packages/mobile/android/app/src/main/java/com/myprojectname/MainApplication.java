f3e3b56179a1672cc44f4adcb5812b9741e26b5a
Bruno Lemos <brunohplemos@gmail.com>
2019-09-06 19:34:19 -0300

    [Android] Fix build (disable autolinking)

--- packages/mobile/android/app/src/main/java/com/myprojectname/MainApplication.java
+++ packages/mobile/android/app/src/main/java/com/myprojectname/MainApplication.java
@@ -1,16 +1,14 @@
 package com.myprojectname;
 
 import android.app.Application;
-import android.util.Log;
 
-import com.facebook.react.PackageList;
-import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
-import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -23,11 +21,14 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      @SuppressWarnings("UnnecessaryLocalVariable")
-      List<ReactPackage> packages = new PackageList(this).getPackages();
+      // @SuppressWarnings("UnnecessaryLocalVariable")
+      //List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
-      return packages;
+      // return packages;
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage()
+      );
     }
 
     @Override
