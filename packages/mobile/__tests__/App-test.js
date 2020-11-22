83d3307783e56412e467bee6981cb73c96624a58
Bruno Lemos <brunohplemos@gmail.com>
2020-11-21 22:39:47 -0300

    Upgrade react-testing-library

--- packages/mobile/__tests__/App-test.js
+++ packages/mobile/__tests__/App-test.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import '@testing-library/jest-native/extend-expect';
-import {render, waitForElement} from '@testing-library/react-native';
+import {render, waitFor} from '@testing-library/react-native';
 
 import {App} from 'components/src/App';
 
 test('Renders the shared app', async () => {
-  const {getByText, baseElement} = render(<App />);
+  const {getByText} = render(<App />);
   const theText = 'Code sharing using Monorepo';
 
-  await waitForElement(() => getByText(theText));
+  await waitFor(() => getByText(theText));
   expect(getByText(theText)).toHaveTextContent(theText);
-  expect(baseElement).toMatchSnapshot();
 });
--- packages/web/src/__tests__/App-test.js
+++ packages/web/src/__tests__/App-test.js
@@ -1,18 +1,13 @@
-/**
- * @format
- */
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { render, waitFor } from "@testing-library/react";
 
-import React from 'react';
-import '@testing-library/jest-dom/extend-expect';
-import { render, waitForElement } from '@testing-library/react';
+import { App } from "components/src/App";
 
-import { App } from 'components/src/App';
+test("Renders the shared app", async () => {
+  const { getByText } = render(<App />);
+  const theText = "Code sharing using Monorepo";
 
-test('Renders the shared app', async () => {
-  const { getByText, baseElement } = render(<App />);
-  const theText = 'Code sharing using Monorepo';
-
-  await waitForElement(() => getByText(theText));
+  await waitFor(() => getByText(theText));
   expect(getByText(theText)).toHaveTextContent(theText);
-  expect(baseElement).toMatchSnapshot();
 });
