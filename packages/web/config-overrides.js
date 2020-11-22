f24b0cb8fc6487c2b5577b34c78a60933b619e8f
Bruno Lemos <brunohplemos@gmail.com>
2020-11-22 02:15:59 -0300

    Upgrade create-react-app to v4.0.0 (and react-native-web to v0.14.8)

--- packages/web/config-overrides.js
+++ packages/web/config-overrides.js
@@ -13,15 +13,16 @@ module.exports = function override(config, env) {
   config.resolve.plugins = config.resolve.plugins.filter(
     (plugin) => plugin.constructor.name !== 'ModuleScopePlugin',
   )
+
   config.module.rules[0].include = appIncludes
-  config.module.rules[1] = null
-  config.module.rules[2].oneOf[1].include = appIncludes
-  config.module.rules[2].oneOf[1].options.plugins = [
+  config.module.rules[1].oneOf[2].include = appIncludes
+  config.module.rules[1].oneOf[2].options.plugins.push(
     require.resolve('babel-plugin-react-native-web'),
-  ].concat(config.module.rules[2].oneOf[1].options.plugins)
-  config.module.rules = config.module.rules.filter(Boolean)
+  )
+
   config.plugins.push(
     new webpack.DefinePlugin({ __DEV__: env !== 'production' }),
   )
+
   return config
 }
